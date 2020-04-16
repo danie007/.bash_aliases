6db0d690beb365fa1f4ba20749a6d607f6427ced
Daniel Selvan D <danilselvan@gmail.com>
2020-04-16 13:19:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,43 +1,15 @@
 #!/bin/bash
 
 ES_SUCCESS=0
-ES_NOPERM=1
+ES_PERM_ERR=1
 
+test_site=google.com
+
+# Check for root previlages
 if [[ $EUID -ne 0 ]]; then
     echo "run the ${!#} as root"
-    exit $ES_NOPERM
+    exit $ES_PERM_ERR
 fi
-
-apt update
-
-# Checking for curl
-which curl &> /dev/null
-if [ $? -ne 0 ]; then
-    echo "Installing curl..."
-    apt install curl -y
-fi
-
-curl https://raw.githubusercontent.com/danie007/.bash_aliases/master/.bash_aliases > ~/.bash_aliases
-curl https://raw.githubusercontent.com/danie007/.bash_aliases/master/.bashrc > ~/.bashrc
-
-if [ -d "/home/$(logname)" ]; then
-    cp ~/.bash_aliases ~/.bashrc /home/$(logname)/ 2> /dev/null
-    chown $(logname): /home/$(logname)/.bash_aliases 2> /dev/null
-    chown $(logname): /home/$(logname)/.bashrc 2> /dev/null
-fi
-
-cd
-# source .bashrc
-
-echo "Setting vm swappiness to 10"
-
-# backing up original configuration
-cp /etc/sysctl.conf /etc/sysctl.conf.orig
-
-# Removing old swappiness, if any and rewritng the file
-grep -v "vm.swappiness" /etc/sysctl.conf > /etc/sysctl.conf 2> /dev/null
-echo -e "# Restricting swappiness\nvm.swappiness=10" >> /etc/sysctl.conf
-sysctl -p
 
 # function to automatically detect the user and environment of a current session
 function run-in-user-session() {
@@ -48,39 +20,101 @@ function run-in-user-session() {
     sudo -Hu "$_username" env "${_environment[@]}" "$@"
 }
 
-# echo "Changing dock position to bottom... (current user: $(sudo -u $SUDO_USER whoami) only)"
-# run-in-user-session gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
-# run-in-user-session gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
-# run-in-user-session gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 24
+# Getting the static hostname of runtime
+S_HOSTNAME=$(hostnamectl | grep "Static hostname:" | rev | cut -d' ' -f1 | rev)
 
-# Installing basic utilities
-echo "Installing basic utilities"
-# apt install -y vim make gcc 
+# Check for internet connection before proceeding with internet based commands
+if ping -q -c 1 -W 1 $test_site > /dev/null; then
 
-# Checking for VS Code
-which code &> /dev/null
-if [ $? -ne 0 ]; then
-    # Installing visual studio code
-    echo "Installing visual studio code"
-    snap install --classic code    # Snap is pre-installed from Ubuntu 16.04
+    apt update
 
-    # Setting VS Code as the default text editor
-    update-alternatives --set editor /usr/bin/code
+    # Checking for curl
+    which curl &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "Installing curl..."
+        apt install curl -y
+    fi
+
+    curl https://raw.githubusercontent.com/danie007/.bash_aliases/master/.bash_aliases > ~/.bash_aliases
+    curl https://raw.githubusercontent.com/danie007/.bash_aliases/master/.bashrc > ~/.bashrc
+
+    if [ "$S_HOSTNAME" = "kali" ]; then
+        cat << EOT >> ~/.bash_aliases
+        
+# For Kali Linux 2020
+
+# ifconfig
+alias ifconf='sudo ifconfig'
+
+# msfconsole & initialization
+alias msfcon='sudo service postgresql start && sudo msfdb init && msfconsole'
+
+# dmesg
+alias dmsg='sudo dmesg'
+EOT
+    fi
+
+    if [ -d "/home/$(logname)" ]; then
+        cp ~/.bash_aliases ~/.bashrc /home/$(logname)/ 2> /dev/null
+        chown $(logname): /home/$(logname)/.bash_aliases 2> /dev/null
+        chown $(logname): /home/$(logname)/.bashrc 2> /dev/null
+    fi
+
+    # Installing basic utilities
+    echo "Installing basic utilities"
+    apt install -y vim make gcc build-essential git
+
+    if [ "$S_HOSTNAME" = "ubuntu" ]; then
+        # Checking for VS Code
+        which code &> /dev/null
+        if [ $? -ne 0 ]; then
+            # Installing visual studio code
+            echo "Installing visual studio code"
+            snap install --classic code    # Snap is pre-installed from Ubuntu 16.04
+
+            # Setting VS Code as the default text editor
+            update-alternatives --set editor /usr/bin/code
+        fi
+
+        # Checking for google chrome
+        which google-chrome &> /dev/null
+        if [ $? -ne 0 ]; then
+            echo "Installing google chrome"
+            curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /tmp/chrome_stable.deb
+
+            apt install /tmp/./chrome_stable.deb
+        fi
+
+        # Setting favorite apps
+        run-in-user-session dconf write /org/gnome/shell/favorite-apps "['google-chrome.desktop', 'gnome-calculator.desktop', 'org.gnome.Terminal.desktop', 'code_code.desktop']"
+    fi
+
+    apt full-upgrade -y
+else
+    echo ""
+	echo "  No network connection available!"
+    echo "  Functionalities will be reduced."
+    echo ""
 fi
 
-# Checking for google chrome
-which google-chrome &> /dev/null
-if [ $? -ne 0 ]; then
-    echo "Installing google chrome"
-    curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /tmp/chrome_stable.deb
+cd
 
-    apt install /tmp/./chrome_stable.deb
+if [ "$S_HOSTNAME" = "ubuntu" ]; then
+    echo "Changing dock position to bottom... (current user: $(sudo -u $SUDO_USER whoami) only)"
+    run-in-user-session gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+    run-in-user-session gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
+    run-in-user-session gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 24
 fi
 
-# Setting favorite apps
-run-in-user-session dconf write /org/gnome/shell/favorite-apps "['google-chrome.desktop', 'gnome-calculator.desktop', 'org.gnome.Terminal.desktop', 'code_code.desktop']"
+echo "Setting vm swappiness to 10"
 
-apt full-upgrade -y
+# backing up original configuration
+cp /etc/sysctl.conf /etc/sysctl.conf.orig
+
+# Removing old swappiness, if any and rewritng the file
+grep -v "vm.swappiness" /etc/sysctl.conf.orig > /etc/sysctl.conf
+echo "vm.swappiness=10   # Restricting swap to 10%" >> /etc/sysctl.conf
+sysctl -p
 
 shutdown -r +1 "System will restart in 1 minute, save your work ASAP"
 
